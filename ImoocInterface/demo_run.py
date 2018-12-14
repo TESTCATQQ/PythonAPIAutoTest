bff79522a18f1e297427b48505adafee105e6268
Jun-WorkPC <376456272@qq.com>
2018-12-14 12:42:02 +0800

    new file:   ImoocInterface/demo_run.py

---
+++ ImoocInterface/demo_run.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+def __init__(self,url,method,data=None):
+    self.res = self.run_main(url,method,data)
+
+class RunMain:
+    def send_post(self,url,data):
+        res = requests.post(url=url,data=data).json()
+        return json.dumps(res,indent=2,sort_keys=True)
+
+    def send_get(self,url,data):
+        res = requests.get(url=url,data=data).json()
+        return json.dumps(res,indent=2,sort_keys=True)
+
+    def run_main(self,url,method,data=None):
+        res = None
+        if method == 'GET':
+            res = self.send_get(url,data)
+        else:
+            res = self.send_post(url,data)
+        return res
+
+if __name__ == "__main__":
+
+    #POST
+    url1 = 'http://127.0.0.1:8000/login/'
+    #GET
+    url2 = 'http://127.0.0.1:8000/loginget/'
+
+    data1 = {
+        'username':'Junjie',
+        'password':'123456'
+    }
+
+    data2 = {
+        'username':'Junjie',
+        'mobile':'123456',
+        'data':'this is data'
+    }
+
+    run = RunMain()
+    print(run.run_main(url1,'POST',data1))
